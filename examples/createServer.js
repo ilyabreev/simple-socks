c2d5f015575b9af1728e3fe9959e88e3414b5809
Ilya Breev <ilyabreev@gmail.com>
2016-02-29 16:11:45 +0300

    Restriction by source addresses. Using ip that client connected to as outgoing interface.

--- examples/createServer.js
+++ examples/createServer.js
@@ -1,40 +1,17 @@
 'use strict';
 
+var argv = require('yargs')
+	.usage('Usage: $0 -p [num] -a [addr1 addr2 addr3]')
+	.demand(['p', 'a'])
+	.argv;
+
 var
 	socks5 = require('../lib'),
-	server = socks5.createServer();
+	server = socks5.createServer({ allowFrom: argv.a });
 
-// start listening!
-server.listen(1080);
+server.listen(argv.p);
 
-server.on('handshake', function (socket) {
-	console.log();
-	console.log('------------------------------------------------------------');
-	console.log('new socks5 client from %s:%d', socket.remoteAddress, socket.remotePort);
-});
-
-// When a reqest arrives for a remote destination
-server.on('proxyConnect', function (info, destination) {
-	console.log('connected to remote server at %s:%d', info.host, info.port);
-
-	destination.on('data', function (data) {
-		console.log(data.length);
-	});
-});
-
-server.on('proxyData', function (data) {
-	console.log(data.length);
-});
-
-// When an error occurs connecting to remote destination
 server.on('proxyError', function (err) {
 	console.error('unable to connect to remote server');
 	console.error(err);
-});
-
-// When a proxy connection ends
-server.on('proxyEnd', function (response, args) {
-	console.log('socket closed with code %d', response);
-	console.log(args);
-	console.log();
 });
